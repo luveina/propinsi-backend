d4261c49eab208ca7c0ac200eb1e379e691a8620
khaynaura <grankhay@gmail.com>
2026-03-01 21:47:52 +0700

    login, register, logout

---
+++ src/main/java/com/propinsi/backend/restdto/request/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.propinsi.backend.restdto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotBlank(message = "Nama Lengkap wajib diisi")
+    private String fullName;
+
+    @NotBlank(message = "Username wajib diisi")
+    @Size(min = 4, message = "Username minimal 4 karakter")
+    private String username;
+
+    @NotBlank(message = "Nomor Telepon wajib diisi")
+    private String phoneNumber;
+
+    @NotBlank(message = "Password wajib diisi")
+    @Size(min = 6, message = "Password minimal 6 karakter")
+    private String password;
+
+    @NotBlank(message = "Konfirmasi Password wajib diisi")
+    private String confirmPassword;
+}
