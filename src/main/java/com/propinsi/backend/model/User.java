c153948d35ac99df3320e5cc658f9db485c8d24f
Rahma Dwi Maghfira <rahma.dwi@ui.ac.id>
2026-03-02 21:24:38 +0700

    nyoba

---
+++ src/main/java/com/propinsi/backend/model/User.java
@@ -0,0 +1,25 @@
+package com.propinsi.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String namaLengkap;
+    private String nomorWhatsApp;
+    private String role;
+    private String password;
+}
