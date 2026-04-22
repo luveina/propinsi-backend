50e78abdac99bd71a1b0be876f5003cc61f6585f
khaynaura <grankhay@gmail.com>
2026-04-22 17:33:33 +0700

    fixing timezone

--- src/main/java/com/propinsi/backend/BackendApplication.java
+++ src/main/java/com/propinsi/backend/BackendApplication.java
@@ -1,11 +1,23 @@
 package com.propinsi.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	@PostConstruct
+    public void init() {
+        // Mengatur default timezone aplikasi ke UTC
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
